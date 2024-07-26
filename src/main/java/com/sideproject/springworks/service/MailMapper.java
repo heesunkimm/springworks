e9b70e0707082ff65bb52ca81ee0d94d86fc10ee
minyoung <minpoooh@gmail.com>
2024-07-26 16:32:02 +0900

    매퍼 주석처리

--- src/main/java/com/sideproject/springworks/service/MailMapper.java
+++ src/main/java/com/sideproject/springworks/service/MailMapper.java
@@ -9,8 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.sideproject.springworks.dto.MailDTO;
 
-import javax.mail.*;
-import javax.mail.internet.MimeMessage;
+
 import java.util.List;
 import java.util.Properties;
 
@@ -29,52 +28,36 @@ public class MailMapper {
 	    private final String username = "your-email@example.com";
 	    private final String password = "your-password";
 
-	    public void checkMail() {
-	        Properties properties = new Properties();
-	        properties.put("mail.store.protocol", "imaps");
-
-	        try {
-	            Session session = Session.getDefaultInstance(properties, null);
-	            Store store = session.getStore("imaps");
-	            store.connect(host, username, password);
-
-	            Folder inbox = store.getFolder("INBOX");
-	            inbox.open(Folder.READ_ONLY);
-
-	            Message[] messages = inbox.getMessages();
-
-	            for (Message message : messages) {
-	                if (message instanceof MimeMessage) {
-	                    MimeMessage mimeMessage = (MimeMessage) message;
-	                    String subject = mimeMessage.getSubject();
-	                    String content = (String) mimeMessage.getContent();
-	                    saveMail(subject, content);
-	                }
-	            }
-
-	            inbox.close(false);
-	            store.close();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    } 
-
-	    public void saveMail(String subject, String content) {
-	    	MailDTO mail = new MailDTO();
-	        mail.setMail_writer(subject);
-	        mail.setContent(content);
-	        mailMapper.insertMail(mail);
-	    }
-	    
-	    public void insertMail(MailDTO mail) {
-	    	
-	        mailMapper.insertMail(mail);
-	    }
-	    
-	    
-
-	    public List<MailDTO> getAllMails() {
-	        return mailMapper.getAllMails();
-	    }
+	/*
+	 * public void checkMail() { Properties properties = new Properties();
+	 * properties.put("mail.store.protocol", "imaps");
+	 * 
+	 * try { Session session = Session.getDefaultInstance(properties, null); Store
+	 * store = session.getStore("imaps"); store.connect(host, username, password);
+	 * 
+	 * Folder inbox = store.getFolder("INBOX"); inbox.open(Folder.READ_ONLY);
+	 * 
+	 * Message[] messages = inbox.getMessages();
+	 * 
+	 * for (Message message : messages) { if (message instanceof MimeMessage) {
+	 * MimeMessage mimeMessage = (MimeMessage) message; String subject =
+	 * mimeMessage.getSubject(); String content = (String) mimeMessage.getContent();
+	 * saveMail(subject, content); } }
+	 * 
+	 * inbox.close(false); store.close(); } catch (Exception e) {
+	 * e.printStackTrace(); } }
+	 * 
+	 * public void saveMail(String subject, String content) { MailDTO mail = new
+	 * MailDTO(); mail.setMail_writer(subject); mail.setContent(content);
+	 * mailMapper.insertMail(mail); }
+	 * 
+	 * public void insertMail(MailDTO mail) {
+	 * 
+	 * mailMapper.insertMail(mail); }
+	 * 
+	 * 
+	 * 
+	 * public List<MailDTO> getAllMails() { return mailMapper.getAllMails(); }
+	 */
 	
 }

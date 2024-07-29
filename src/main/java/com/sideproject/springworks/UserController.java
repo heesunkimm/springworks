af8bad83dabb47f89e2b1b89267e3013197a787c
user <khsun4624@naver.com>
2024-07-29 17:04:00 +0900

    [수정][works jsp, 유저 jps, mapper, controller, css]

--- src/main/java/com/sideproject/springworks/UserController.java
+++ src/main/java/com/sideproject/springworks/UserController.java
@@ -1,14 +1,17 @@
 package com.sideproject.springworks;
 
+import java.time.LocalDate;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sideproject.springworks.dto.UserDTO;
 import com.sideproject.springworks.service.UserMapper;
@@ -35,5 +38,21 @@ public class UserController {
 		
 		return "user/works/works";
 	}
+	
+//	@PostMapping("/create_userId.ajax")
+//	@ResponseBody
+//	public String createUserId() {
+//		LocalDate today = LocalDate.now();
+//		System.out.println(today);
+//		String userId = "";
+//		
+//		
+//		return "";
+//	}
+//	
+//	@RequestMapping("/create_user.do")
+//	public String createUser(HttpServletRequest req, UserDTO dto) {
+//		return "redirect:works";
+//	}
 	
 }
